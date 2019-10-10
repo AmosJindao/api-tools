f799190468cd8af290ddaaca1aa9f67b851eba46
Shengri Liu <sheliu@fortinet.com>
2019-10-10 14:28:40 -0700

    m wrapper

--- src/main/java/com/api/tools/ApiUtils.java
+++ src/main/java/com/api/tools/ApiUtils.java
@@ -1,9 +1,23 @@
 package com.api.tools;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Author: srliu
  * Date: 10/5/19
  */
 public class ApiUtils {
+    public String getValue(String jsonData, String filedPath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(new ByteArrayInputStream(jsonData.getBytes(StandardCharsets.UTF_8)));
 
+
+        return null;
+    }
 }
---
+++ src/main/test/com/api/tools/ApiUtilsTests.java
@@ -0,0 +1,13 @@
+package com.api.tools;
+
+/**
+ * @author shengri
+ * @date 10/10/19
+ */
+public class ApiUtilsTests {
+
+    @Test
+    public void getFieldValue(){
+
+    }
+}
